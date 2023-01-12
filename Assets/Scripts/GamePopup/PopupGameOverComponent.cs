c77df35e086d0ed4c958fd2960529691a196c842
Grosheva Daria <dariagrosheva99@gmail.com>
2023-01-12 19:11:28 +0400

    Chached popup game over

--- Assets/Scripts/GamePopup/PopupGameOverComponent.cs
+++ Assets/Scripts/GamePopup/PopupGameOverComponent.cs
@@ -7,14 +7,14 @@ namespace GamePopup
     {
         private GameModel _gameModel;
         private GameScene _gameScene;
-        
+
         public GameObject PopupGameOver;
 
         public override void Initialize(GameModel gameModel, GameScene gameScene)
         {
             _gameModel = gameModel;
             _gameScene = gameScene;
-            
+
             _gameModel.FailedBalls.Changed += OnFailedBallsChanged;
         }
 
@@ -25,10 +25,9 @@ namespace GamePopup
 
         private void OnFailedBallsChanged()
         {
-            if (_gameModel.FailedBalls.Value >= _gameScene.FailedBallsLooseValue)
+            if (_gameModel.GameState.Value == GameState.GameOver)
             {
                 PopupGameOver.SetActive(true);
-                _gameModel.GameState.Value = GameState.GameOver;
             }
         }
     }
